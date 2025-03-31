5a07c7a1731f80c0468b32519a99c421397e7764
SoumyaDashore <sdashore124@gmail.com>
2025-03-31 19:31:57 +0530

    updated

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -61,6 +61,22 @@ export const About = () => {
                   ))}
                 </div>
               </div>
+
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Other</h3>
+                <div className="flex flex-wrap gap-2">
+                  {otherSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
 
